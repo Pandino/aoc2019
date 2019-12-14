97e56ae3f3bc1d4cd5840b96048c36b0f4504ca2
Giorgio Ceolin <giorgio.ceolin@crealogix.com>
2019-12-14 11:51:33 +0100

    Day 12 Part 2 not working

--- 12/part1.py
+++ 12/part1.py
@@ -1,5 +1,6 @@
 import re
 from itertools import combinations
+from math import gcd
 
 aoc_input='''<x=16, y=-8, z=13>
 <x=4, y=10, z=10>
@@ -20,11 +21,19 @@ class Moon():
     def __init__(self, position, velocity=None):
         self.position = position
         self.velocity = [0, 0, 0]
+        self.orbital_period = None
+
     def apply_velocity(self):
         self.position = [position + velocity for position, velocity in zip(self.position, self.velocity)]
 
     def total_energy(self):
         return sum([abs(i) for i in self.position]) * sum([abs(i) for i in self.velocity])
+
+    def __hash__(self):
+        return hash((tuple(self.position), tuple(self.velocity)))
+
+    def __eq__(self, other):
+        return tuple(self.position) == tuple(other.position) and tuple(self.velocity) == tuple(other.velocity)
 
     def __repr__(self):
         return f'pos={self._print_vector(self.position)}, vel={self._print_vector(self.velocity)})'
@@ -37,10 +46,20 @@ class Simulation():
         self.steps = 0
 
     def run(self, steps):
+        original_state = [hash(moon) for moon in self.moons]
         while self.steps < steps:
+            if all([moon.orbital_period is not None for moon in self.moons]):
+                break
             self._apply_gravity()
             self._apply_velocity()
             self.steps += 1
+            for n, moon in enumerate(self.moons):
+                if hash(moon) == original_state[n]:
+                    if moon.orbital_period is None:
+                        print(f'Moon {n} original state at {self.steps}')
+                        moon.orbital_period = self.steps
+        else:
+            print('Couldn\'t find all original states')
 
     def total_energy(self):
         return sum([moon.total_energy() for moon in self.moons])
@@ -59,6 +78,15 @@ class Simulation():
         for moon in self.moons:
             moon.apply_velocity()
 
+def lcm(values):
+    def _lcm(a, b):
+        return a * b // gcd(a, b)
+    result = 1
+    for value in values:
+        result = _lcm(result, value)
+    return result
+
+
 if __name__ == '__main__':
     moons = list()
     for line in aoc_input.splitlines():
@@ -70,7 +98,12 @@ if __name__ == '__main__':
             raise Exception('Error parsing: ' + line)
 
     simulation = Simulation(moons)
-    simulation.run(1000)
-    for moon in simulation.moons:
-        print(moon)
-    print(simulation.total_energy())
+    simulation.run(100000000000)
+    if any([moon.orbital_period is None for moon in moons]):
+        print('Skip')
+    else:
+        part2 = lcm([moon.orbital_period for moon in moons])
+        print(f'Steps to initial state: {part2}')
+    # for moon in moons:
+        # print(moon.orbital_period)
+    # print(simulation.total_energy())
