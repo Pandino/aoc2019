ee5b937dfc473df375ec2ac3e2f6439326a42562
Giorgio Ceolin <giorgio.ceolin@crealogix.com>
2019-12-14 16:12:01 +0100

    Day 13 part 2 done (urgh)

--- 13/part1.py
+++ 13/part1.py
@@ -1,5 +1,6 @@
 from intcode import cpu
 from blessed import Terminal
+from time import sleep
 
 def import_code(filename):
     code = list()
@@ -23,19 +24,33 @@ class Arcade():
             if state[0] == 0:
                 break
             elif state[0] == 1:
-                raise NotImplementedError()
+                with self.terminal.cbreak():
+                    val = self.terminal.inkey(timeout=0)
+                    if val == 'a':
+                        self.cpu.input(-1)
+                    elif val == 'd':
+                        self.cpu.input(1)
+                    elif val == 'q':
+                        break
+                    else:
+                        self.cpu.input(0)
             elif state[0] == 2:
                 self.output_buffer.append(state[1])
-                if len(self.output_buffer) >= 3:
-                    x, y, tile = self.output_buffer[0:3]
+                if len(self.output_buffer) == 3:
+                    x, y, tile = self.output_buffer
                     self.update_screen(x, y, tile)
                     self.output_buffer = list()
 
     def update_screen(self, x, y, tile):
-        print(self.terminal.move(y, x) + self.tile_map[tile], end='')
+        if x == -1:
+            print(self.terminal.move(0, 0) + str(tile))
+        else:
+            y += 1
+            print(self.terminal.move(y, x) + self.tile_map[tile], end='')
 
 if __name__ == '__main__':
     code = import_code('13/input.int')
+    code[0] = 2
     game = Arcade(code)
     game.run()
     print()
